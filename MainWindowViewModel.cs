714a101b01c7b12babd78855cad43bd15e34363e
Akira Kubo <a_kubo99@yahoo.co.jp>
2018-04-20 17:03:14 +0900

    ﻿:+1: 機能改善 add Property

--- MainWindowViewModel.cs
+++ MainWindowViewModel.cs
@@ -1,12 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Prism.Mvvm;
 
 namespace WpfAppErrorHandling
 {
-    class MainWindowViewModel
+    class MainWindowViewModel:INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler PropertyChanged;
+        private double input1_;
+        public double Input1
+        {
+            get { return input1_; }
+            set
+            {
+                input1_ = value;
+                OnPropertyChanged("Input1");
+            }
+        }
+        private void OnPropertyChanged(string name)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(name));
+            }
+        }
     }
 }
